55d14db80bad0a9ef46acd99223f0678ecef63d5
EndersonMarques <EndersonMarques>
2022-03-10 14:30:37 -0300

    apanahndo com a auestao da luquet :/

--- 2021-2/lista-04/Textos_Napoleônicos.c
+++ 2021-2/lista-04/Textos_Napoleônicos.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    char texto[501];
+    char texto[501], numeroChareado[4] = {0};
     int i, j, k;
+    int cMai = 0, cMine = 0;
     fgets(texto, 501, stdin);
+
+    // percorre todo texto
     for (i = 0; texto[i] != '\0'; i++)
     {
+        // encontra o primeio [xx-]
         if (texto[i] == '[')
         {
+            // vai ate o final do primero [xx-]]
             for (j = i + 1; texto[j] != ']'; j++)
             {
                 int numero[3];
                 int c = 0;
+                // econtra o primero numero cha forma ['x', 'x']
                 while (texto[j] != '-')
                 {
                     // printf("%c", texto[j]);
@@ -28,10 +36,42 @@ int main()
                     valor += numero[k] * pow(10, temp);
                     temp--;
                 }
-                printf("%c", valor);
+                // printf("%c", valor);
+                // Adcionando ao vetor novamento
+                int l = 0;
+                numeroChareado[l] = valor;
+                l++;
+
+                // Verificando quantos maisuclos / minusculos existem e tratando
+                for (int m = 0; m < l; m++)
+                {
+                    // printf("%c", numeroChareado[m]);
+                    if (numeroChareado[m] < 97)
+                    {
+                        // printf("Contou maisuculo : %c\n", numeroChareado[m]);
+                        cMai++;
+                    }
+                    else
+                    {
+                        // printf("Contou minuscuo : %c\n", numeroChareado[m]);
+                        cMine++;
+                    }
+                    for (int a = 0; a < l; a++)
+                    {
+                        printf("%c", numeroChareado[a]);
+                    }
+                }
             }
-            printf("\n");
+            // printf("\n");
+            printf("%d %d\n", cMai, cMine);
+            // zerando
+            cMine = 0;
+            cMai = 0;
         }
+        // if ((texto[i] != '[' && texto[i] != ']' && texto[i] != '-') && !(texto[i] >= 48 && texto[i] <= 59))
+        // {
+        //     printf("%c", texto[i]);
+        // }
     }
     return 0;
 }
